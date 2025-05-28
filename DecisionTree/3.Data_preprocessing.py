1f356e5124cb4040b9a210442e1420111377a77e
송영우 <lesong0929@gmail.com>
2025-05-28 13:30:02 +0900

    Add data preprocessing script for Decision Tree

---
+++ DecisionTree/3.Data_preprocessing.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+df = pd.read_csv("DS_term/2_Cleaning_DataSet.csv", low_memory=False)
+
+df_normal = df[df['Label'] == 0].copy()
+df_attack = df[df['Label'] == 1].copy()
+
+
+categorical_cols = df.select_dtypes(include=['object']).columns.tolist()
+categorical_cols = [col for col in categorical_cols if col not in ['Label']]
+for col in categorical_cols:
+    le = LabelEncoder()
+    df[col] = le.fit_transform(df[col])
+
+
+df.to_csv("DS_term/DecisionTree/3_Preprocessing_DataSet.csv", index=False)
+
